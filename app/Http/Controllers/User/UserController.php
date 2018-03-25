d73c201c848e9db3d1f671dcfd9ebb1320c27adb
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-03-25 09:44:49 +0700

    Add verify method

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -106,7 +106,7 @@ class UserController extends ApiController
 
         if ($request->has('admin')) {
             $this->allowedAdminAction();
-            
+
             if (!$user->isVerified()) {
                 return $this->errorResponse('Only verified users can modify the admin field', 409);
             }
@@ -134,6 +134,15 @@ class UserController extends ApiController
         $user->delete();
 
         return $this->showOne($user);
+    }
+
+    public function verify($token)
+    {
+        $user = User::where('verification_token', $token)->firstOrFail();
+        $user->verified = User::VERIFIED_USER;
+        $user->verification_token = null;
+        $user->save();
+        return $this->showMessage('The account has been verified successfully');
     }
 
     public function resend(User $user)
